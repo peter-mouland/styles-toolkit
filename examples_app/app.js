22d9537a43e2b6b2feda68afc7032e8f5b024817
Ashish Sehra, Rich Matthews and Rob Jones <ashishsehra+richmatthews+robertjones@notonthehighstreet.com>
2016-02-17 14:20:37 +0000

    remove iframed sections

--- examples_app/app.js
+++ examples_app/app.js
@@ -4,7 +4,6 @@ var path = require('path');
 
 app.use('/dist', express.static(path.join(__dirname, '../', 'dist')));
 app.use('/styles', express.static(__dirname + '/styles'));
-app.use('/examples', express.static(__dirname + '/examples'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
