d97fc1dddf0454d5dffe14ca935b5ddc1c33e668
Peter Mouland <peter.mouland@gmail.com>
2016-07-21 11:05:07 +0100

    base64 encode woff files to reduce downloads and fout

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  name: 'styles-toolkit',
+  entry: {
+    'dist/toolkit': ["./styles/toolkit.scss"]
+  },
+  output: {
+    path: path.join(__dirname),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css")
+  ],
+  resolve: {
+    modulesDirectories: [ 'node_modules', './styles' ]
+  },
+  module: {
+    loaders: [
+      { test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", [ "css", "postcss", "sass" ].join("!"))
+      },
+      { test: /\.woff(2)?$/, loader: "url?mimetype=application/font-woff" }
+    ]
+  },
+  postcss: [autoprefixer({
+        browsers: [
+          'safari 9',
+          'ie 10-11',
+          'last 2 Chrome versions',
+          'last 2 Firefox versions',
+          'edge 13',
+          'ios_saf 9.0-9.2',
+          'ie_mob 11',
+          'Android >= 4'
+        ],
+        cascade: false,
+        add: true,
+        remove: true
+      }
+  )]
+};
