6546674cd5355fbd3b0916b6ccdf19babc34cfa0
Peter Mouland <peter.mouland@gmail.com>
2016-10-17 16:36:59 +0100

    Docs (#45)
    * improve docs ui

--- examples_app/app.js
+++ examples_app/app.js
@@ -3,7 +3,9 @@ var app = express();
 var path = require('path');
 
 app.use('/dist', express.static(path.join(__dirname, '../', 'dist')));
+app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/styles', express.static(__dirname + '/styles'));
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
